3892177bb45267cd856e7582c10d5da3eb257d47
Erik Minarini <minarini.work@gmail.com>
2017-08-11 23:42:28 +0200

    camera control

--- tmp/main.js
+++ tmp/main.js
@@ -8,11 +8,11 @@ window.addEventListener('DOMContentLoaded', function() {
 
 	    var scene = new BABYLON.Scene(engine);
 
-	    var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5,-10), scene);
+	    var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5,-5), scene);
 
 	    camera.setTarget(BABYLON.Vector3.Zero());
 
-	    camera.attachControl(canvas, false);
+	    //camera.attachControl(canvas, false);
 
 	 
 	    var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
